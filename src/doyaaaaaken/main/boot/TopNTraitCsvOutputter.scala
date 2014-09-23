b6908b0c305cbaef2f64dffdfad3e10f897441c7
doyaaaaaken <kk03140816@gmail.com>
2014-09-23 19:57:04 +0900

    Top40の様式リストを出力できるようにした。

--- src/doyaaaaaken/main/boot/TopNTraitCsvOutputter.scala
+++ src/doyaaaaaken/main/boot/TopNTraitCsvOutputter.scala
@@ -1,6 +1,9 @@
 package doyaaaaaken.main.boot
 
 import java.io.PrintWriter
+import doyaaaaaken.main.db.DbSession
+import doyaaaaaken.model.TraitFreqHistory
+import doyaaaaaken.model.TraitFreqHistoryDataRow
 
 /**
  * trait_freq_historyテーブルよりデータ取得し
@@ -8,6 +11,28 @@ import java.io.PrintWriter
  */
 private[boot] object TopNTraitCsvOutputter extends PrintWriterUser {
   override def csvOutput(pw: PrintWriter): Unit = {
-    //TODO 未実装
+    val con = DbSession.getConnection
+    val topNTraitsData: Seq[TraitFreqHistoryDataRow] = TraitFreqHistory.selectTopNTraitsData(con)
+
+    //1行目を項目行として作成
+    pw.print("timeStep,")
+    for (i <- 1 to 16382) pw.print(i + ",")
+    pw.println
+
+    //2行目以下にデータを挿入
+    topNTraitsData.groupBy(_.timestep).foreach {
+      case (timeStep, dataRow) =>
+        {
+          var tmpTraitKindCounter = 1 //","の表示制御に使うための一時変数
+          pw.print(timeStep + ",")
+          dataRow.sortBy(_.trait_kind).foreach {
+            case row =>
+              for (i <- 1 to row.trait_kind - tmpTraitKindCounter) pw.print(",")
+              tmpTraitKindCounter = row.trait_kind + 1
+              pw.print(row.freq + ",")
+          }
+        }
+        pw.println
+    }
   }
 }
--- src/doyaaaaaken/model/TraitFreqHistory.scala
+++ src/doyaaaaaken/model/TraitFreqHistory.scala
@@ -6,6 +6,7 @@ import java.sql.ResultSet
 import java.sql.SQLException
 import java.sql.Statement
 import doyaaaaaken.main.boot.Property
+import scala.collection.mutable.ListBuffer
 
 /**
  * 各ターンにおける様式の度数を表すクラス
@@ -103,4 +104,43 @@ object TraitFreqHistory {
     }
     datas
   }
+
+  /**
+   * TopNの様式の度数推移だけをtrait_freq_historyテーブルから抜き出す
+   */
+  def selectTopNTraitsData(con: Connection): Seq[TraitFreqHistoryDataRow] = {
+    val datas: ListBuffer[TraitFreqHistoryDataRow] = ListBuffer()
+    try {
+      val stmt: Statement = con.createStatement
+      val dbTableName: String = Property.dbName + "." + Property.traitFreqHistoryTableName
+      val sql: StringBuilder = new StringBuilder
+      sql.append("SELECT * FROM ")
+      sql.append(dbTableName)
+      sql.append(" AS tfh1 WHERE tfh1.trait_kind IN (SELECT tfh2.trait_kind FROM (SELECT tfh3.trait_kind FROM ")
+      sql.append(dbTableName)
+      sql.append(" AS tfh3 GROUP BY tfh3.trait_kind ORDER BY MAX(tfh3.freq) DESC LIMIT 40) AS tfh2);")
+
+      val rs: ResultSet = stmt.executeQuery(sql.toString())
+
+      while (rs.next()) {
+        val id: Int = rs.getInt("id")
+        val timeStep: Int = rs.getInt("timestep")
+        val traitKind: Int = rs.getInt("trait_kind")
+        val freq: Int = rs.getInt("freq")
+        datas += TraitFreqHistoryDataRow(id, timeStep, traitKind, freq)
+      }
+      rs.close
+      stmt.close
+    } catch {
+      case e: SQLException => println("Database error " + e)
+      case e: Throwable => {
+        println("Some other exception type on DbSession:")
+        e.printStackTrace
+      }
+    }
+    datas.toList
+  }
 }
+
+/**trait_freq_historyテーブルからSELECTした1行を格納するDTO*/
+case class TraitFreqHistoryDataRow(id: Int, timestep: Int, trait_kind: Int, freq: Int)
