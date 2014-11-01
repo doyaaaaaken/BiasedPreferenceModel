94a898af6101dcbf834cd5da5cb8270fd615f56d
doyaaaaaken <kk03140816@gmail.com>
2014-11-01 19:39:39 +0900

    一時置き。実験途中である。

--- src/doyaaaaaken/main/boot/csvoutputter/TopNTraitCsvOutputter.scala
+++ src/doyaaaaaken/main/boot/csvoutputter/TopNTraitCsvOutputter.scala
@@ -29,30 +29,30 @@ private[boot] object TopNTraitCsvOutputter extends PrintWriterUser {
         val traitKindsList: Seq[Int] = topNTraitsDatas.map(_.trait_kind).toSet.toList.sorted //データとして存在したtrait_kind群
         val traitKindsPosition: Map[Int, Int] = { for ((x, i) <- traitKindsList.zipWithIndex) yield (x, i + 2) }.toMap //「._2列目がtraitKind ._1番目の列」という情報
 
-        //タイトル行の出力
-        pw.println(simNum + "回目のシミュレーション結果")
-
-        //2行目の出力
-        pw.print("timeStep")
-        traitKindsList.foreach(traitKind => pw.print("," + traitKind))
-        pw.println
-
-        //3行目以下にデータを挿入
-        topNTraitsDatas.groupBy(_.timestep).foreach {
-          case (timeStep, dataRows) =>
-            {
-              var tmpTraitKindCounter = 1 //","の表示制御に使うための一時変数
-              pw.print(timeStep)
-              dataRows.sortBy(_.trait_kind).foreach {
-                case row =>
-                  for (i <- 1 to traitKindsPosition.apply(row.trait_kind) - tmpTraitKindCounter) pw.print(",")
-                  pw.print(row.freq)
-                  tmpTraitKindCounter = traitKindsPosition.apply(row.trait_kind)
-              }
-            }
-            pw.println
-        }
-        pw.println
+        //        //タイトル行の出力
+        //        pw.println(simNum + "回目のシミュレーション結果")
+        //
+        //        //2行目の出力
+        //        pw.print("timeStep")
+        //        traitKindsList.foreach(traitKind => pw.print("," + traitKind))
+        //        pw.println
+        //
+        //        //3行目以下にデータを挿入
+        //        topNTraitsDatas.groupBy(_.timestep).foreach {
+        //          case (timeStep, dataRows) =>
+        //            {
+        //              var tmpTraitKindCounter = 1 //","の表示制御に使うための一時変数
+        //              pw.print(timeStep)
+        //              dataRows.sortBy(_.trait_kind).foreach {
+        //                case row =>
+        //                  for (i <- 1 to traitKindsPosition.apply(row.trait_kind) - tmpTraitKindCounter) pw.print(",")
+        //                  pw.print(row.freq)
+        //                  tmpTraitKindCounter = traitKindsPosition.apply(row.trait_kind)
+        //              }
+        //            }
+        //            pw.println
+        //        }
+        //        pw.println
 
         //全様式数のうちのhopedな様式の数を末尾に出力する
         val sb: StringBuffer = new StringBuffer()
@@ -63,7 +63,7 @@ private[boot] object TopNTraitCsvOutputter extends PrintWriterUser {
         sb.append(",)な様式の数は  ,")
         sb.append(traitKindsList.filter(_ % Property.hopedTraitGenerateInterval == 0).size)
         pw.println(sb.toString())
-        pw.println
+      //        pw.println
     }
   }
 
